e1305075d5c9c58522ec71ab1c592dab8e9147a5
Himanshu Lohaniwal <lohaniwalhimanshu@gmail.com>
2020-12-23 15:06:26 +0530

    First commit

---
+++ Factorise.py
@@ -0,0 +1,6 @@
+print('Hi!! This is a program that will factorial the number you will tell me ..')
+num1 = int(input('Please Enter The Number:'))
+num2 = 1
+for i in range(1, num1+1):
+    num2 = num2 * i
+print('Your Answer is :', num2)
